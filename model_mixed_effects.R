ee657e8883c2dca34894457557fb237c9d63d45a
nevao <nevao@uw.edu>
2024-12-05 11:23:41 -0800

    deleted commented out scaling text

--- model_mixed_effects.R
+++ model_mixed_effects.R
@@ -17,12 +17,6 @@ ibis_behav$Group[ibis_behav$Group == ""] <- NA
 # Remove rows with no Group
 ibis_behav_filtered <- ibis_behav[!is.na(ibis_behav$Group), ]
 
-source("scale_columns_to_zscore.R")
-
-# # Scale score columns
-# ibis_behav_filtered <- scale_columns_to_zscore(ibis_behav_filtered_unscaled, 
-#           c("AB_12_Percent", "AB_24_Percent", "Flanker_Standard_Age_Corrected", "DCCS_Standard_Age_Corrected"))
-
 # Convert 'Group' to a factor
 ibis_behav_filtered$Group <- factor(ibis_behav_filtered$Group)
 

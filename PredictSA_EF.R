bdfbe0456f15d138b0e674cce0bf7832f73cf782
nevao <nevao@uw.edu>
2025-02-18 13:19:14 -0800

    Read in file that has Identifier - CandID link instead of anotb file

--- PredictSA_EF.R
+++ PredictSA_EF.R
@@ -6,11 +6,14 @@ rm(list = ls())
 demopath <- "/Users/nevao/Documents/IBIS_EF/source data/"
 brainpath <- "/Users/nevao/Documents/IBIS_EF/source data/Brain_Data/"
 
+ids <- 
+  read.csv(paste0(demopath, "IBIS IDs.csv"))
+
 demo_behav <- 
   read.csv(paste0(demopath, "IBIS_behav_dataframe_demographics_AnotB_Flanker_DCCS_BRIEF2.csv"))
 
-anotb <- 
-  read.csv(paste0(demopath, "AnotB_clean.csv"))
+#anotb <- 
+#  read.csv(paste0(demopath, "AnotB_clean.csv"))
 
 cortex_parcel_df <- 
   read.csv(paste0(brainpath, "IBISandDS_VSA_Cerebrum_and_LobeParcel_v01.02_20210809/",
@@ -26,9 +29,20 @@ subcort_df <-
 
 source("Utility_Functions.R")
 
-anotb <- anotb[c("Identifiers", "DCCID")]
-names(anotb)[names(anotb) == "DCCID"] <- "CandID"
-anotb$CandID<- as.integer(anotb$CandID)
+ids_new <- ids %>%
+  mutate(
+    V06.demographcs.CandID = ifelse(V06.demographics.CandID == "." | V06.demographics.CandID == "", NA, V06.demographics.CandID),
+    VSA.demographics.CandID = ifelse(VSA.demographics.CandID == "." | VSA.demographics.CandID == "", NA, VSA.demographics.CandID),
+    V06.demographics.CandID = suppressWarnings(as.numeric(V06.demographics.CandID)),
+    VSA.demographics.CandID = suppressWarnings(as.numeric(VSA.demographics.CandID)),
+    CandID = coalesce(V06.demographics.CandID, VSA.demographics.CandID)
+  ) %>%
+  select(Identifiers, CandID)
+
+
+#ids <- ids[c("Identifiers", "DCCID")]
+#names(ids)[names(ids) == "DCCID"] <- "CandID"
+#anotb$CandID<- as.integer(anotb$CandID)
 
 demo_behav[demo_behav == ""] <- NA
 
@@ -48,3 +62,5 @@ merged_df <- merged_df %>%
   filter(rowSums(!is.na(select(., -Identifiers, -CandID, -Combined_ASD_DX, -Risk, -Group, -Sex))) > 0)
 
 merged_df <- merged_df[, !(grepl("Age", names(merged_df)) & !grepl("Age_Corrected", names(merged_df)))]
+
+

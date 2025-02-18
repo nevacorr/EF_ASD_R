069824dd92f388e39a380ee8d2597c0156d89414
nevao <nevao@uw.edu>
2025-02-18 13:25:34 -0800

    finalized integrating new file that matches identifier with CandID

--- PredictSA_EF.R
+++ PredictSA_EF.R
@@ -12,9 +12,6 @@ ids <-
 demo_behav <- 
   read.csv(paste0(demopath, "IBIS_behav_dataframe_demographics_AnotB_Flanker_DCCS_BRIEF2.csv"))
 
-#anotb <- 
-#  read.csv(paste0(demopath, "AnotB_clean.csv"))
-
 cortex_parcel_df <- 
   read.csv(paste0(brainpath, "IBISandDS_VSA_Cerebrum_and_LobeParcel_v01.02_20210809/",
                  "IBISandDS_VSA_Cerebrum_and_LobeParcel_Vols_v01.02_20210706.csv"))
@@ -29,20 +26,16 @@ subcort_df <-
 
 source("Utility_Functions.R")
 
-ids_new <- ids %>%
+# Change ids file so that it has only Identifiers and CandID columns that represents values for all ages
+ids <- ids %>%
   mutate(
     V06.demographcs.CandID = ifelse(V06.demographics.CandID == "." | V06.demographics.CandID == "", NA, V06.demographics.CandID),
     VSA.demographics.CandID = ifelse(VSA.demographics.CandID == "." | VSA.demographics.CandID == "", NA, VSA.demographics.CandID),
-    V06.demographics.CandID = suppressWarnings(as.numeric(V06.demographics.CandID)),
-    VSA.demographics.CandID = suppressWarnings(as.numeric(VSA.demographics.CandID)),
+    V06.demographics.CandID = suppressWarnings(as.integer(V06.demographics.CandID)),
+    VSA.demographics.CandID = suppressWarnings(as.integer(VSA.demographics.CandID)),
     CandID = coalesce(V06.demographics.CandID, VSA.demographics.CandID)
   ) %>%
   select(Identifiers, CandID)
-
-
-#ids <- ids[c("Identifiers", "DCCID")]
-#names(ids)[names(ids) == "DCCID"] <- "CandID"
-#anotb$CandID<- as.integer(anotb$CandID)
 
 demo_behav[demo_behav == ""] <- NA
 
@@ -50,10 +43,9 @@ cortex_parcel_df <- remove_bad_quality_data(cortex_parcel_df)
 vol_df <- remove_bad_quality_data(vol_df)
 subcort_df <- remove_bad_quality_data(subcort_df)
 
-
 demo_behav <- subset(demo_behav, select = -X)
 
-merged_df <- merge(anotb, demo_behav, by = "Identifiers", all.x = TRUE, all.y = TRUE)
+merged_df <- merge(ids, demo_behav, by = "Identifiers", all.x = TRUE, all.y = TRUE)
 merged_df <- merge(merged_df, cortex_parcel_df, by = "CandID", all.x = TRUE, all.y = FALSE)
 merged_df <- merge(merged_df, vol_df, by = "CandID", all = TRUE, all.x = TRUE, all.y = FALSE)
 merged_df <- merge(merged_df, subcort_df, by = 'CandID', all = TRUE, all.x = TRUE, all.y = FALSE)

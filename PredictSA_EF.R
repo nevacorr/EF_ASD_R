d5c3dd79cee48d2ba6629b24f40ee0689abc395d
nevao <nevao@uw.edu>
2025-02-18 16:59:13 -0800

    Change input brain data from SA to 12 and 24mo. Import all behavior and volumetric cortex data into dataframe and divide all brain volumes by totTiss.

--- PredictSA_EF.R
+++ PredictSA_EF.R
@@ -4,7 +4,7 @@ library(dplyr)
 rm(list = ls())
 
 demopath <- "/Users/nevao/Documents/IBIS_EF/source data/"
-brainpath <- "/Users/nevao/Documents/IBIS_EF/source data/Brain_Data/"
+brainpath <- "/Users/nevao/Documents/IBIS_EF/source data/Brain_Data/IBIS1&2_volumes_v3.13/"
 
 ids <- 
   read.csv(paste0(demopath, "IBIS IDs.csv"))
@@ -13,16 +13,13 @@ demo_behav <-
   read.csv(paste0(demopath, "IBIS_behav_dataframe_demographics_AnotB_Flanker_DCCS_BRIEF2.csv"))
 
 cortex_parcel_df <- 
-  read.csv(paste0(brainpath, "IBISandDS_VSA_Cerebrum_and_LobeParcel_v01.02_20210809/",
-                 "IBISandDS_VSA_Cerebrum_and_LobeParcel_Vols_v01.02_20210706.csv"))
+  read.csv(paste0(brainpath, "IBIS_v3.13_LobeParcel_2020May5_withFrontDiv_V12V24only.csv"))
 
-vol_df <- 
-  read.csv(paste0(brainpath, "IBISandDS_VSA_TissueSeg_v01.02_20210706/",
-                  "IBISandDS_VSA_TissueSeg_Vols_v01.02_20210706.csv"))
+icv_df <- 
+  read.csv(paste0(brainpath, "IBIS_v3.13_ICV_2020May4_V12V24only.csv"))
 
-subcort_df <- 
-  read.csv(paste0(brainpath, "IBISandDS_VSA_Subcort_and_LV_v01.02_20210706/",
-                  "IBISandDS_VSA_Subcort_and_LV_Vols_v01.02_20210706.csv"))
+tissue_df <- 
+  read.csv(paste0(brainpath, "IBIS_v3.13_TotTiss_2020May4_V12V24only.csv"))
 
 source("Utility_Functions.R")
 
@@ -39,20 +36,32 @@ ids <- ids %>%
 
 demo_behav[demo_behav == ""] <- NA
 
-cortex_parcel_df <- remove_bad_quality_data(cortex_parcel_df)
-vol_df <- remove_bad_quality_data(vol_df)
-subcort_df <- remove_bad_quality_data(subcort_df)
+source("Utility_Functions.R")
+
+cortex_parcel_df <- remove_bad_quality_data_12_24(cortex_parcel_df, c('Par', 'Front', 'Cing'))
+icv_df <- remove_bad_quality_data_12_24(icv_df, c('ICV'))
+tissue_df <- remove_bad_quality_data_12_24(tissue_df, c('totTiss'))
 
 demo_behav <- subset(demo_behav, select = -X)
 
 merged_df <- merge(ids, demo_behav, by = "Identifiers", all.x = TRUE, all.y = TRUE)
 merged_df <- merge(merged_df, cortex_parcel_df, by = "CandID", all.x = TRUE, all.y = FALSE)
-merged_df <- merge(merged_df, vol_df, by = "CandID", all = TRUE, all.x = TRUE, all.y = FALSE)
-merged_df <- merge(merged_df, subcort_df, by = 'CandID', all = TRUE, all.x = TRUE, all.y = FALSE)
+merged_df <- merge(merged_df, icv_df, by = "CandID", all = TRUE, all.x = TRUE, all.y = FALSE)
+merged_df <- merge(merged_df, tissue_df, by = 'CandID', all = TRUE, all.x = TRUE, all.y = FALSE)
 
 merged_df <- merged_df %>%
   filter(rowSums(!is.na(select(., -Identifiers, -CandID, -Combined_ASD_DX, -Risk, -Group, -Sex))) > 0)
 
 merged_df <- merged_df[, !(grepl("Age", names(merged_df)) & !grepl("Age_Corrected", names(merged_df)))]
 
+merged_df <- merged_df[, !grepl("Reversal", colnames(merged_df))]
 
+for (col in grep("GM|WM.*_V12$|GM|WM.*_V24$", colnames(merged_df), value = TRUE)) {
+  merged_df[[col]] <- as.numeric(merged_df[[col]])
+  
+  if (grepl("_V12$", col)) {
+    merged_df[[col]] <- merged_df[[col]] / merged_df$totTiss_V12
+  } else if (grepl("_V24$", col)) {
+    merged_df[[col]] <- merged_df[[col]] / merged_df$totTiss_V24
+  }
+}
--- Utility_Functions.R
+++ Utility_Functions.R
@@ -1,4 +1,7 @@
 
+library(dplyr)
+library(tidyr)
+
 # Plot histogram of a dataframe column
 plot_histogram <- function(df, column_name) {
   # Check if the column exists in the dataframe
@@ -57,4 +60,47 @@ remove_bad_quality_data <- function(df) {
   
   # Remove columns that contain "VQC" or "Exclude_Reason"
   df <- df[, !grepl("VQC|Exclude_Reason|Visit", colnames(df))]
+}
+
+remove_bad_quality_data_12_24 <- function(df, region_substrings) {
+  
+  df[df == "."] <- NA
+  
+  qc_cols <- grep("_VQC$", names(df), value = TRUE)
+  
+  for (i in seq_along(region_substrings)) {
+    region_matches <- grep(region_substrings[i], names(df), value = TRUE)
+    region_matches <- region_matches[!grepl("VQC", region_matches)]
+    qc_match <- grep(paste0(region_substrings[i], ".*_VQC$"), names(df), value = TRUE)
+    
+    for (region_col in region_matches) {
+      df[[region_col]][df[[qc_match]] == "fail"] <- NA
+    }
+  }
+  
+  # Remove columns that will not be used in modeling
+  df <- df[, !grepl("VQC|ExcludeReason|Occ|Temp|Edited|Combined", colnames(df))]
+  
+  # Find all column names that contain any of the region substrings
+  var_names <- grep(paste(region_substrings, collapse = "|"), names(df), value = TRUE)
+  
+  # Ensure that we found columns
+  if (length(var_names) == 0) {
+    stop("No columns found matching the specified substrings.")
+  }
+  
+  # Make separate columns for 12 and 24 month data
+  df <- df %>%
+    pivot_wider(
+      names_from = Visit,  # Spread the "Visit" values into columns
+      values_from = all_of(var_names),  # Use the columns that match the substrings
+      names_glue = "{.value}_{Visit}"  # New column names with Visit suffix
+    )
+  
+  # Rename DCCID column
+  df <- df %>%
+    rename(CandID = DCCID)
+  
+  
+  return(df)
 }
